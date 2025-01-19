6e62975fd7114a30c8b3fcbb18e8e2239a63f365
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 14:29:05 +0900

    refactor(#master): amountFor 함수 추출

--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
+import { Performance, Play } from "./type";
+
+function amountFor(perf: Performance, play: Play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy": // 비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+
+    case "comedy": // 희극
+      thisAmount = 30000;
+      if (perf.audience > 30) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르 ${play.type}`);
+  }
+
+  return thisAmount;
+}
+
 function statement(invoice: any, plays: any) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -10,7 +37,7 @@ function statement(invoice: any, plays: any) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
     switch (play.type) {
       case "tragedy": // 비극
