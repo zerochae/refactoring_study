0038c3720dc5dcb2cda38f8e93305a38db65a1a8
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 14:35:00 +0900

    refactor(#master): inline variable play in amountFor funciton

--- index.ts
+++ index.ts
@@ -1,33 +1,33 @@
 import { Performance, Play, Plays } from "./type";
 
-function amountFor(aPerformance: Performance, play: Play) {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-
-    case "comedy": // 희극
-      result = 30000;
-      if (aPerformance.audience > 30) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르 ${play.type}`);
-  }
-
-  return result;
-}
-
 function statement(invoice: any, plays: Plays) {
   function playFor(aPerformance: Performance) {
     return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance: Performance, play: Play) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 30) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르 ${playFor(aPerformance).type}`);
+    }
+
+    return result;
   }
 
   let totalAmount = 0;
