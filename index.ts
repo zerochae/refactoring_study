f8174bc566f151f548750808496ec52ed2548606
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 14:51:04 +0900

    refactor(#master): 함수 추출 totalAmount

--- index.ts
+++ index.ts
@@ -57,16 +57,23 @@ function statement(invoice: Invoice, plays: Plays) {
     return volumeCredits;
   }
 
-  let totalAmount = 0;
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+
+    return result;
+  }
+
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
