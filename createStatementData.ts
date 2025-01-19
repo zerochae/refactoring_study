0cc959b7f92af8e65eeffe7488c4c6102a9df563
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 15:33:48 +0900

    feat(#master): calculator class

--- createStatementData.ts
+++ createStatementData.ts
@@ -1,4 +1,49 @@
-import { Invoice, Performance, Plays, Statement } from "./type";
+import { Invoice, Performance, Plays, Statement, Play } from "./type";
+
+class PerformanceCalculator {
+  performance: Performance;
+  play: Play;
+
+  constructor(aPerformance: Performance, aPlay: Play) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+
+      case "comedy": // 희극
+        result = 30000;
+        if (this.performance.audience > 30) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르 ${this.performance.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.performance.play.type) {
+      result += Math.floor(this.performance.audience / 5);
+    }
+
+    return result;
+  }
+}
 
 export default function createStatementData(invoice: Invoice, plays: Plays) {
   const statementData = {} as Statement;
@@ -10,50 +55,16 @@ export default function createStatementData(invoice: Invoice, plays: Plays) {
   return statementData;
 
   function enrichPerformance(aPerformance: Performance) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance) as any;
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance: Performance) {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance: Performance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 30) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(perf: Performance) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === perf.play.type) {
-      result += Math.floor(perf.audience / 5);
-    }
-
-    return result;
   }
 
   function totalVolumeCredits(data: Statement) {
