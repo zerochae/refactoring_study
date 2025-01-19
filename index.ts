3ae93a2fc3bff7337b9934dd14c633f90706b664
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 14:53:06 +0900

    refactor(#master): 함수 추출 renderPlainText

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import { Invoice, Performance, Plays } from "./type";
 
-function statement(invoice: Invoice, plays: Plays) {
+function renderPlainText(invoice: Invoice, plays: Plays) {
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+  for (let perf of invoice.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
+
   function playFor(aPerformance: Performance) {
     return plays[aPerformance.playID];
   }
@@ -65,17 +73,10 @@ function statement(invoice: Invoice, plays: Plays) {
 
     return result;
   }
+}
 
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
-  for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-  }
-
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
+function statement(invoice: Invoice, plays: Plays) {
+  return renderPlainText(invoice, plays);
 }
 
 export { statement };
