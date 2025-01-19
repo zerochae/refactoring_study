13e5e24e391089df016fd84791fde9064828103b
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 15:22:28 +0900

    refactor(#master): renderPlainText -> renderHTML

--- index.ts
+++ index.ts
@@ -1,15 +1,7 @@
 import createStatementData from "./createStatementData";
 import { Invoice, Plays, Statement } from "./type";
 
-function renderPlainText(data: Statement) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-  return result;
-
+function statement(invoice: Invoice, plays: Plays) {
   function usd(aNumber: number) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -17,10 +9,22 @@ function renderPlainText(data: Statement) {
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
   }
-}
 
-function statement(invoice: Invoice, plays: Plays) {
-  return renderPlainText(createStatementData(invoice, plays));
+  function renderHTML(data: Statement) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1/>\n`;
+    result += "<table>\n";
+    result += `<tr><th>연극</th><th>좌석 수</th><th>rmador</th></tr>`;
+    for (let perf of data.performances) {
+      result += `<tr><td>${perf.play.name}</tr></td>`;
+      result += `<tr><td>${perf.audience}석)</tr></td>`;
+      result += `<tr><td>${usd(perf.amount)}</tr></td>`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  }
+
+  return renderHTML(createStatementData(invoice, plays));
 }
 
 export { statement };
