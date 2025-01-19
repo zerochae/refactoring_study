3cb967318d843e097b331a187bb9b05277cf6f69
GrayKwon <gray.kwon@wirebarley.com>
2025-01-19 15:13:08 +0900

    refactor(#master): use reduce functio

--- index.ts
+++ index.ts
@@ -79,21 +79,11 @@ function statement(invoice: Invoice, plays: Plays) {
   }
 
   function totalVolumeCredits(data: Statement) {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
-
-    return volumeCredits;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data: Statement) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 }
 
